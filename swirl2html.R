c811af0dfaa73eb4468ca5483c1cc469fb05a259
Nick Carchedi <nick.carchedi@gmail.com>
2014-06-17 07:16:40 -0400

    tweak swirl2html to source initLesson and load swirl package

--- swirl2html.R
+++ swirl2html.R
@@ -8,6 +8,10 @@ makechunk <- function(item) {
   paste("```{r}", item, "```", sep="\n")
 }
 
+makechunk_noecho <- function(item) {
+  paste("```{r, echo=FALSE}", item, "```", sep="\n")
+}
+
 makemult <- function(item) {
   answers <- unlist(str_split(item, ";"))
   answers <- str_trim(answers)
@@ -40,10 +44,18 @@ makemd.mult_question <- function(unit) {
 }
 
 # Set lesson path
-les <- yaml.load_file("~/Desktop/test.yaml")
+lespath <- "swirl_courses/R_Programming/Apply_Family_of_Functions_-_Part_1/lesson.yaml"
+les <- yaml.load_file(lespath)
 les <- les[-1] # Remove meta
 # Set dest path
-dest <- "~/Desktop/test.md"
+dest <- "~/Desktop/applyfam.Rmd"
+# Get initLesson.R info and write no echo chunk
+initpath <- file.path(dirname(lespath), "initLesson.R")
+initcode <- paste(readLines(initpath, warn=FALSE), collapse="\n")
+initcode <- paste("suppressPackageStartupMessages(library(swirl))", 
+                  initcode, sep="\n")
+cat(makechunk_noecho(initcode), "\n\n", file=dest)
+# Write the rest of the content
 for(unit in les) {
   class(unit) <- unit[['Class']]
   out <- paste(makemd(unit), "\n\n")
