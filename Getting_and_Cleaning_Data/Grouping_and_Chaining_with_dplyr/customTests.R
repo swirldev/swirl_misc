0f08b9eff973763b4fe5690c552eddb6a09c3435
Nick Carchedi <nick.carchedi@gmail.com>
2014-07-21 19:26:02 -0400

    more progress on dplyr pt 2

--- Getting_and_Cleaning_Data/Grouping_and_Chaining_with_dplyr/customTests.R
+++ Getting_and_Cleaning_Data/Grouping_and_Chaining_with_dplyr/customTests.R
@@ -1,1 +1,18 @@
 AUTO_DETECT_NEWVAR <- FALSE
+
+summarize1_test <- function() {
+  # Get e and the script contents
+  e <- get("e", parent.frame())
+  contents <- e$script_contents
+
+  # Put expression in e$expr
+  e$expr <- try(parse(text = contents)[[1]], silent = TRUE)
+
+  # Dispatch to omnitest
+  omnitest('popular <- summarize(by_package,
+                     count = n(),
+                     unique = n_distinct(ip_id),
+                     countries = n_distinct(country),
+                     avg_bytes = mean(size))'
+           )
+}
