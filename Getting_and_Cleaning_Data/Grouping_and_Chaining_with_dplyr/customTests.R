ab9a34ea68feb27a9ba2a1f3af89dc309d605021
Nick Carchedi <nick.carchedi@gmail.com>
2014-07-21 09:08:35 -0400

    add expr_creates_var tests where appropriate

--- Getting_and_Cleaning_Data/Grouping_and_Chaining_with_dplyr/customTests.R
+++ Getting_and_Cleaning_Data/Grouping_and_Chaining_with_dplyr/customTests.R
@@ -1,7 +1,1 @@
-# Put custom tests in this file.
-
-# Uncommenting the following line of code will disable
-# auto-detection of new variables and thus prevent swirl from
-# repeating execution of printing and plotting commands.
-
 AUTO_DETECT_NEWVAR <- FALSE
