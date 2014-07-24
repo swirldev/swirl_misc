4dc6edba8bbc3397b007418308e649979572f06b
Nick Carchedi <nick.carchedi@gmail.com>
2014-07-24 10:22:14 -0400

    some progress on lubridate

--- Getting_and_Cleaning_Data/Dates_and_Times_with_lubridate/initLesson.R
+++ Getting_and_Cleaning_Data/Dates_and_Times_with_lubridate/initLesson.R
@@ -1,4 +1,21 @@
-# Code placed in this file fill be executed every time the
-# lesson is started. Any variables created here will show up in
-# the user's working directory and thus be accessible to them
-# throughout the lesson.
+dt1 <- '1985-08-25' # most common - big to small
+dt2 <- '1985/08/25' # alternative
+dt3 <- '85-8-25' # make sure you specify year fully
+
+# Also handles vectors of dates
+dt4 <- c('2014-05-14', '2014-09-22')
+
+# Often in the US, we refer to mm/dd/yyyy
+dt5 <- '08.25.1985'
+dt6 <- 'Aug 25, 1985'
+dt7 <- 08251985
+
+# Do whatever you want
+dt8 <- '25 August 1985'
+dt9 <- '25081985'
+
+# But be careful, it's not magic!
+dt10 <- '1985825'
+
+
+dt11 <- '2014-08-23 17:23:02'
