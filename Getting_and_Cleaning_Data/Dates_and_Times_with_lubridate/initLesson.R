81ff38f32561dcb4956b9c3e59f3884eb8a74c95
Nick Carchedi <nick.carchedi@gmail.com>
2014-07-28 18:43:26 -0400

    some more progress on lubridate

--- Getting_and_Cleaning_Data/Dates_and_Times_with_lubridate/initLesson.R
+++ Getting_and_Cleaning_Data/Dates_and_Times_with_lubridate/initLesson.R
@@ -1,21 +1,5 @@
-dt1 <- '1985-08-25' # most common - big to small
-dt2 <- '1985/08/25' # alternative
-dt3 <- '85-8-25' # make sure you specify year fully
+# Handles time info with ymd_hms()
+dt1 <- '2014-08-23 17:23:02'
 
-# Also handles vectors of dates
-dt4 <- c('2014-05-14', '2014-09-22')
-
-# Often in the US, we refer to mm/dd/yyyy
-dt5 <- '08.25.1985'
-dt6 <- 'Aug 25, 1985'
-dt7 <- 08251985
-
-# Do whatever you want
-dt8 <- '25 August 1985'
-dt9 <- '25081985'
-
-# But be careful, it's not magic!
-dt10 <- '1985825'
-
-
-dt11 <- '2014-08-23 17:23:02'
+# Also handles vectors of date-times
+dt2 <- c('2014-05-14', '2014-09-22', '2014-07-11')
