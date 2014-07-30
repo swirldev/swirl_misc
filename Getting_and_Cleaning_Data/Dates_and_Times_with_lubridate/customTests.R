9859c5b2daae153b1cd0eba57699adb3144f9b8d
Nick Carchedi <nick.carchedi@gmail.com>
2014-07-30 10:47:48 -0400

    some tweaks and a new custom test for lubridate

--- Getting_and_Cleaning_Data/Dates_and_Times_with_lubridate/customTests.R
+++ Getting_and_Cleaning_Data/Dates_and_Times_with_lubridate/customTests.R
@@ -1,12 +1,15 @@
-# Put custom tests in this file.
-
-# Uncommenting the following line of code will disable
-# auto-detection of new variables and thus prevent swirl from
-# executing every command twice, which can slow things down.
-
-# AUTO_DETECT_NEWVAR <- FALSE
-
-# However, this means that you should detect user-created
-# variables when appropriate. The answer test, creates_new_var()
-# can be used for for the purpose, but it also re-evaluates the
-# expression which the user entered, so care must be taken.
+func_uses_args <- function(...) {
+  e <- get("e", parent.frame())
+  # Get user's expression
+  expr <- e$expr
+  # Capture names of correct args
+  correct_args <- list(...)
+  # Check for the presence of each correct arg in the expr names
+  match_found <- try(sapply(correct_args, function(arg) arg %in% names(expr)))
+  # If something is weird, return FALSE
+  if(!all(is.logical(match_found))) {
+    return(FALSE)
+  }
+  # Did we find all desired args?
+  all(match_found)
+}
