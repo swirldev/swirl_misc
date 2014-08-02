e80f1cc9648ccd4c9a8c3ea619c0fac9ee4c20b2
Nick Carchedi <nick.carchedi@gmail.com>
2014-08-02 14:23:37 -0400

    more progress on tidyr

--- Getting_and_Cleaning_Data/Tidying_Data_with_tidyr/customTests.R
+++ Getting_and_Cleaning_Data/Tidying_Data_with_tidyr/customTests.R
@@ -1,12 +1,54 @@
-# Put custom tests in this file.
+match_call <- function(correct_call = NULL) {
+  e <- get("e", parent.frame())
+  # Trivial case
+  if(is.null(correct_call)) return(TRUE)
+  # Get full correct call
+  full_correct_call <- expand_call(correct_call)
+  # Expand user's expression
+  expr <- deparse(e$expr)
+  full_user_expr <- try(expand_call(expr), silent = TRUE)
+  # Check if expansion went okay
+  if(is(full_user_expr, "try-error")) return(FALSE)
+  # Compare function calls with full arg names
+  identical(full_correct_call, full_user_expr)
+}
 
-# Uncommenting the following line of code will disable
-# auto-detection of new variables and thus prevent swirl from
-# executing every command twice, which can slow things down.
+# Utility function for match_call answer test
+# Fills out a function call with full argument names
+expand_call <- function(call_string) {
+  # Quote expression
+  qcall <- parse(text=call_string)[[1]]
+  # If expression is not greater than length 1...
+  if(length(qcall) <= 1) return(qcall)
+  # See if it's an assignment
+  is_assign <- is(qcall, "<-")
+  # If assignment, process righthandside
+  if(is_assign) {
+    # Get righthand side
+    rhs <- qcall[[3]]
+    # If righthand side is not a call, can't use match.fun()
+    if(!is.call(rhs)) return(qcall)
+    # Get function from function name
+    fun <- match.fun(rhs[[1]])
+    # match.call() does not support primitive functions
+    if(is.primitive(fun)) return(qcall)
+    # Get expanded call
+    full_rhs <- match.call(fun, rhs)
+    # Full call
+    qcall[[3]] <- full_rhs
+  } else { # If not assignment, process whole thing
+    # Get function from function name
+    fun <- match.fun(qcall[[1]])
+    # match.call() does not support primitive functions
+    if(is.primitive(fun)) return(qcall)
+    # Full call
+    qcall <- match.call(fun, qcall)
+  }
+  # Return expanded function call
+  qcall
+}
 
-# AUTO_DETECT_NEWVAR <- FALSE
-
-# However, this means that you should detect user-created
-# variables when appropriate. The answer test, creates_new_var()
-# can be used for for the purpose, but it also re-evaluates the
-# expression which the user entered, so care must be taken.
+prints_var <- function(varname) {
+  e <- get("e", parent.frame())
+  any_of_exprs(varname, paste0('print(', varname, ')'))
+}
